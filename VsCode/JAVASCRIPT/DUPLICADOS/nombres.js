7112a1331f74ed3886fbd8d5f1c6a8af20e7f3a3
jesus1daw <jesuslucas2004@gmail.com>
2024-11-04 12:42:06 +0100

    04/11

---
+++ VsCode/JAVASCRIPT/DUPLICADOS/nombres.js
@@ -0,0 +1,21 @@
+/*const nombres=new Set(["Pepe","Juan","Pedro"]);
+console.log(nombres.size);
+console.log(nombres.clear());
+console.log(nombres);
+nombres.add("Jesus");
+nombres.add("Ivan");
+nombres.add("Khaled");
+console.log(nombres);
+console.log(nombres.has("Jesus"));*/
+
+
+nombres=["Antonio","Pedro","Lucas"];
+names= new Set([]);
+names.add(nombres[0]);
+names.add(nombres[1]);
+names.add(nombres[2]);
+console.log(names);
+console.log(nombres);
+
+nombres2=[...names];
+console.log(nombres2);
