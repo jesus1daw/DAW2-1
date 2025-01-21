e4fdcd2415d95113caf8291f0acedf06efc56d97
jesus1daw <jesuslucas2004@gmail.com>
2025-01-21 13:16:49 +0100

    21/1

--- VsCode/JSP-Servlets/ProyectoCookiesConPreferencia/src/main/java/PaqueteServlets/recepcion.java
+++ VsCode/JSP-Servlets/ProyectoCookiesConPreferencia/src/main/java/PaqueteServlets/recepcion.java
@@ -10,6 +10,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
+import org.apache.el.lang.FunctionMapperImpl.Function;
+
 import java.sql.*;
 /**
  * Servlet implementation class recepcion
@@ -41,53 +44,26 @@ public class recepcion extends HttpServlet {
 		
 		String nombre=request.getParameter("nombre");
 		String clave=request.getParameter("clave");
-		Boolean error=false;
 		String mensajeError="";
 		
 		
-		if(nombre.length()<6 || nombre.length()>30) {
-			error=true;
-			mensajeError="error1";
-		} 
-		if(!nombre.matches("[a-zA-Z0-9_-]+")){
-			error=true;
-			mensajeError="error2";
-		}
-		if (clave.length() < 6 || clave.length() > 30) {
-		    error = true; // Longitud no válida
-		    mensajeError="error3";
-		}
-		if (!clave.matches(".*[A-Z].*")) {
-		    error = true; // No contiene al menos una letra mayúscula
-		    mensajeError="error4";
-		}
-		if (!clave.matches(".*[a-z].*")) {
-		    error = true; // No contiene al menos una letra minúscula
-		    mensajeError="error5";
-		}
-		if (!clave.matches(".*[0-9].*")) {
-		    error = true; // No contiene al menos un número
-		    mensajeError="error6";
-		}
-		if (!clave.matches(".*[\\-_\\/()$%&?!+*#<>=].*")) {
-		    error = true; // No contiene al menos un carácter especial permitido
-		    mensajeError="error7";
-		}
-		
-		
-		if(error==true) {
+		if(validarUser(nombre) || validarClave(clave)) {
+
+		mensajeError="Fallo en la validacion";
 		request.getSession().setAttribute("mensajeError", mensajeError);
 		response.sendRedirect("index.jsp");
+		
 		} else {
 			
 				String userName = "admin1";
 		        String password = "admin1";
-		        String url = "jdbc:mysql://localhost:3306/Tienda";
+		        String url = "jdbc:mysql://localhost:3306/tienda";
+		        
 		        try {
 		            
 		            Class.forName("com.mysql.cj.jdbc.Driver");
 		            Connection conn = DriverManager.getConnection(url, userName, password);
-		            response.getWriter().append("Conexion exitosa");
+		            response.getWriter().append("Conexion exitosa ");
 		           
 		            String sqlStr = "SELECT * FROM usuarios WHERE usuario = ? AND contraseña = ? ";
 		            PreparedStatement consulta=conn.prepareStatement(sqlStr);
@@ -97,34 +73,74 @@ public class recepcion extends HttpServlet {
 		            ResultSet resultado=consulta.executeQuery();
 		            
 		            if (resultado.next()) {
-		                response.getWriter().append("Usuario encontrado \r\n");
+		                response.getWriter().append("Usuario encontrado  ");
 		            } else {
-		                response.getWriter().append("Usuario no encontrado \r\n");
+		                response.getWriter().append("Usuario no encontrado  ");
 		            }
 		            
 		        } catch (ClassNotFoundException e) {
 		            // Manejar excepción si el driver no se encuentra
-		            System.out.println("Error: No se encontró el driver JDBC.");
+		        	response.getWriter().append("No se encuentra el driver ");
 		            e.printStackTrace();
 		        } catch (SQLException e) {
 		            // Manejar errores relacionados con la base de datos
-		            System.out.println("Error: No se pudo conectar a la base de datos.");
+		        	response.getWriter().append("Fallo al conectar con la base de datos ");
 		            e.printStackTrace();
 		        } catch (Exception e) {
 		            // Capturar cualquier otra excepción
-		            System.out.println("Error inesperado.");
+		        	response.getWriter().append("Error ");
 		            e.printStackTrace();
 		        }
 		}
-			
-
-			
-		
-		
-			
-		
 		
 	}
+	
+	
+	public boolean validarUser(String nombre) {
+		
+		boolean error=false;
+		
+		if(nombre.length()<6 || nombre.length()>30) {
+			error=true;
+		} 
+		
+		if(!nombre.matches("[a-zA-Z0-9_-]+")){
+			error=true;
+		}
+		
+		return error;
+		
+	}
+	
+	
+	public boolean validarClave(String clave) {
+		
+		boolean error=false;
+		
+		if (clave.length() < 6 || clave.length() > 30) {
+		    error = true; // Longitud no válida   
+		}
+		
+		if (!clave.matches(".*[A-Z].*")) {
+		    error = true; // No contiene al menos una letra mayúscula 
+		}
+		
+		if (!clave.matches(".*[a-z].*")) {
+		    error = true; // No contiene al menos una letra minúscula
+		}
+		
+		if (!clave.matches(".*[0-9].*")) {
+		    error = true; // No contiene al menos un número
+		}
+		
+		if (!clave.matches(".*[\\-_\\/()$%&?!+*#<>=].*")) {
+		    error = true; // No contiene al menos un carácter especial permitido 
+		}
+		
+		return error;
+	}
+	
+	
 }
 
 	
